202612da3c196b0c4e30d56fa61372df88ca25ac
camillejackson205 <39915134+camillejackson205@users.noreply.github.com>
2020-05-04 18:16:03 -0400

    austin is da best

---
+++ build/precache-manifest.d49d81ae7a6a647ef99957f6c38cdc4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "924ad79a525372bb24b75500d1ada628",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a7c18471feae729e9f1",
+    "url": "/static/css/main.93477050.chunk.css"
+  },
+  {
+    "revision": "53d086d36406671fcf55",
+    "url": "/static/js/2.71fe1d07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.71fe1d07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a7c18471feae729e9f1",
+    "url": "/static/js/main.1d4c5936.chunk.js"
+  },
+  {
+    "revision": "747728924604c2c7cb8c",
+    "url": "/static/js/runtime-main.7741fc59.js"
+  }
+]);
